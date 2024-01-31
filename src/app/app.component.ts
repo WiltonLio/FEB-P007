7f6ab16cc66918607a4feecae722e534448cc462
WiltonLio <wilton.lio@gmail.com>
2024-01-31 15:49:36 -0300

    update app.component.css, app.component.html, app.component.spec.ts and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FEB-P007';
+  categoriaSelecionada: string | null = null;
+  veiculos: { Name: string }[] = [];
+  veiculoSelecionado: string | null = null;
+  nomesVeiculos: string[] = [];
+  tituloSelecionado: string | null = null;
+  mostrarAdicionarObjeto: boolean = false;
+  exibirBotoesRodape: boolean = false;
+
+  onCategoriaSelecionada(categoria: string) {
+    this.categoriaSelecionada = categoria;
+  }
+
+  onVeiculosSelecionados(veiculos: { Name: string }[]) {
+    this.veiculos = veiculos;
+  }
+
+  onVeiculoSelecionado(veiculo: string) {
+    this.veiculoSelecionado = veiculo;
+  }
+
+  onNomesVeiculosSelecionados(nomesVeiculos: string[]) {
+    // Implemente a lógica necessária aqui
+  }
+
+  onTituloSelecionado(titulo: string) {
+    this.tituloSelecionado = titulo;
+    this.mostrarAdicionarObjeto = true;
+  }
+
+  onAdicionarAoCarrinhoClicado(nomeVeiculo: string) {
+    // Lógica para quando o botão "Adicionar ao carrinho" for clicado
+    // Não há console.log aqui
+
+    // Adiciona o nome do veículo à lista de nomesVeiculos
+    this.nomesVeiculos.push(nomeVeiculo);
+
+    // Define a visibilidade dos botões no rodapé como true
+    this.exibirBotoesRodape = true;
+  }
 }
